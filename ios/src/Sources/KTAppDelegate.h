0edde225be721e6523b34d6b4fa5e38e733e0025
Akun <245811423+haizarakun@users.noreply.github.com>
2026-09-06 23:34:36 +0900

    Move iOS build to its own repository (haizarakun/koetomo-ios)

--- ios/src/Sources/KTAppDelegate.h
+++
@@ -1,5 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface KTAppDelegate : UIResponder <UIApplicationDelegate>
-@property (nonatomic, strong) UIWindow *window;
-@end
--- ios/src/Sources/KTViewController.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <WebKit/WebKit.h>
-
-@interface KTViewController : UIViewController <WKScriptMessageHandler, WKUIDelegate, WKNavigationDelegate>
-@property (nonatomic, strong) WKWebView *webView;
-- (void)handleIncomingURL:(NSURL *)url;
-@end
